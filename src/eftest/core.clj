bc5f9c67d7d54d3f61aea20b7c47b8c4f22e5090
James Reeves <jreeves@weavejester.com>
2015-08-10 16:20:43 +0100

    Add find-tests-in-dir function

--- src/eftest/core.clj
+++ src/eftest/core.clj
@@ -3,7 +3,14 @@
             [clojure.test :as test]
             [clojure.tools.namespace.find :as find]))
 
+(defn require-namespaces-in-dir [dir]
+  (map (fn [ns] (require ns) (find-ns ns)) (find/find-namespaces-in-dir (io/file dir))))
+
+(defn find-tests-in-namespace [ns]
+  (->> ns ns-interns vals (filter (comp :test meta))))
+
+(defn find-tests-in-dir [dir]
+  (mapcat find-tests-in-namespace (require-namespaces-in-dir dir)))
+
 (defn test-dir [dir]
-  (doseq [ns (find/find-namespaces-in-dir (io/file dir))]
-    (require ns)
-    (test/test-ns ns)))
+  (test/test-vars (find-tests-in-dir dir)))
