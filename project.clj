7ec932d0ebb16577b0e529992c3dfc91cb8de58b
James Reeves <jreeves@weavejester.com>
2015-08-10 22:01:24 +0100

    Add progress bar to test reporting

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.namespace "0.2.11"]])
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [progrock "0.1.0"]])
--- src/eftest/core.clj
+++ src/eftest/core.clj
@@ -1,7 +1,8 @@
 (ns eftest.core
   (:require [clojure.java.io :as io]
             [clojure.test :as test]
-            [clojure.tools.namespace.find :as find]))
+            [clojure.tools.namespace.find :as find]
+            [progrock.core :as prog]))
 
 (defn require-namespaces-in-dir [dir]
   (map (fn [ns] (require ns) (find-ns ns)) (find/find-namespaces-in-dir (io/file dir))))
@@ -11,9 +12,6 @@
 
 (defn find-tests-in-dir [dir]
   (mapcat find-tests-in-namespace (require-namespaces-in-dir dir)))
-
-(defn test-dir [dir]
-  (test/test-vars (find-tests-in-dir dir)))
 
 (defonce clojure-report-methods (methods test/report))
 
@@ -26,3 +24,31 @@
 
 (doseq [[type method] clojure-report-methods]
   (defmethod test/report type [m] (*report* m)))
+
+(declare ^:dynamic *progress*)
+
+(defmulti progress-report (fn [_ m] (:type m)))
+
+(defmethod progress-report :default [bar m])
+
+(defmethod progress-report :pass [bar m]
+  (prog/print (swap! bar prog/tick)))
+
+(defmethod progress-report :fail [bar m]
+  (prog/print (swap! bar prog/tick)))
+
+(defmethod progress-report :error [bar m]
+  (prog/print (swap! bar prog/tick)))
+
+(defmethod progress-report :summary [bar m]
+  (prog/print (swap! bar prog/done)))
+
+(defn wrap-progress-bar [f]
+  (fn [vars]
+    (let [bar (atom (prog/progress-bar (count vars)))]
+      (binding [*report* (partial progress-report bar)]
+        (prog/print @bar)
+        (f vars)))))
+
+(defn test-dir [dir]
+  ((wrap-progress-bar test/test-vars) (find-tests-in-dir dir)))
