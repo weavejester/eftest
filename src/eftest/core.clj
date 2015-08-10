575b28fa40db229c4a9b350f78561f8f267fc2e9
James Reeves <jreeves@weavejester.com>
2015-08-10 04:34:50 +0100

    Add test-dir function

--- src/eftest/core.clj
+++ src/eftest/core.clj
@@ -1,6 +1,9 @@
-(ns eftest.core)
+(ns eftest.core
+  (:require [clojure.java.io :as io]
+            [clojure.test :as test]
+            [clojure.tools.namespace.find :as find]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn test-dir [dir]
+  (doseq [ns (find/find-namespaces-in-dir (io/file dir))]
+    (require ns)
+    (test/test-ns ns)))
