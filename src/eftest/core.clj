e76153fd774e9b0ff9abb2860d135b2347f87466
James Reeves <jreeves@weavejester.com>
2015-08-16 16:06:47 +0100

    Run tests in parallel

--- src/eftest/core.clj
+++ src/eftest/core.clj
@@ -1,9 +1,9 @@
 (ns eftest.core
   (:require [clojure.java.io :as io]
             [clojure.test :as test]
-            [clojure.tools.namespace.find :as find]
-            [progrock.core :as prog]
-            [io.aviso.ansi :as ansi]))
+            [clojure.tools.namespace.find :as find]))
+
+(defmethod test/report :begin-test-run [_])
 
 (defn require-namespaces-in-dir [dir]
   (map (fn [ns] (require ns) (find-ns ns)) (find/find-namespaces-in-dir (io/file dir))))
@@ -14,11 +14,26 @@
 (defn find-tests-in-dir [dir]
   (mapcat find-tests-in-namespace (require-namespaces-in-dir dir)))
 
-(defmethod test/report :begin-test-run [_])
+(defn test-ns-vars [ns vars]
+  (binding [test/*report-counters* (ref test/*initial-report-counters*)]
+    (test/report {:type :begin-test-ns, :ns ns})
+    (test/test-vars vars)
+    (test/report {:type :end-test-ns, :ns ns})
+    @test/*report-counters*))
+
+(defn locking-report [report]
+  (let [lock (Object.)]
+    (fn [m] (locking lock (report m)))))
 
 (defn test-vars [vars]
   (test/report {:type :begin-test-run, :vars vars})
-  (test/test-vars vars))
+  (let [report   (locking-report test/report)
+        counters (pmap (fn [[ns vars]]
+                         (binding [test/report report]
+                           (test-ns-vars ns vars)))
+                       (group-by (comp :ns meta) vars))]
+    (test/report (-> (apply merge-with + counters)
+                     (assoc :type :summary)))))
 
 (defn test-dir [dir]
   (test-vars (find-tests-in-dir dir)))
