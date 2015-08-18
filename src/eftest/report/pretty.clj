30761a7c7219a0d0de2cc1fb0429baefab2c91c1
James Reeves <jreeves@weavejester.com>
2015-08-18 20:05:24 +0100

    Add customizable fonts for pretty reporter

--- src/eftest/report/pretty.clj
+++ src/eftest/report/pretty.clj
@@ -4,9 +4,26 @@
             [io.aviso.exception :as exception]
             [io.aviso.repl :as repl]))
 
+(def ^:dynamic *fonts*
+  {:exception      ansi/red-font
+   :reset          ansi/reset-font
+   :message        ansi/italic-font
+   :property       ansi/bold-font
+   :source         ansi/italic-font
+   :namespace-name ansi/reset-font
+   :function-name  ansi/blue-font
+   :clojure-frame  ansi/yellow-font
+   :java-frame     ansi/white-font
+   :omitted-frame  ansi/reset-font
+   :pass           ansi/green-font
+   :fail           ansi/red-font
+   :error          ansi/red-font})
+
 (defn testing-vars-str [{:keys [file line]}]
-  (str (ansi/bold (-> test/*testing-vars* first str (subs 2)))
-       " (" file ":" line ")"))
+  (let [test-var (first test/*testing-vars*)]
+    (str (:namespace-name *fonts*) (-> test-var meta :ns ns-name) (:reset *fonts*) "/"
+         (:function-name *fonts*) (-> test-var meta :name) (:reset *fonts*)
+         (:source *fonts*) " (" file ":" line ")" (:reset *fonts*))))
 
 (defmulti report :type)
 
@@ -19,7 +36,7 @@
   (test/with-test-out
     (test/inc-report-counter :fail)
     (newline)
-    (println (str (ansi/red "FAIL") " in") (testing-vars-str m))
+    (println (str (:fail *fonts*) "FAIL" (:reset *fonts*) " in") (testing-vars-str m))
     (when (seq test/*testing-contexts*) (println (test/testing-contexts-str)))
     (when message (println message))
     (println "expected:" (pr-str expected))
@@ -29,20 +46,20 @@
   (test/with-test-out
    (test/inc-report-counter :error)
    (newline)
-   (println (str (ansi/red "ERROR") " in") (testing-vars-str m))
+   (println (str (:error *fonts*) "ERROR" (:reset *fonts*) " in") (testing-vars-str m))
    (when (seq test/*testing-contexts*) (println (test/testing-contexts-str)))
    (when message (println message))
    (println "expected:" (pr-str expected))
    (print "  actual: ")
    (if (instance? Throwable actual)
-     (binding [exception/*traditional* true]
+     (binding [exception/*traditional* true, exception/*fonts* *fonts*]
        (repl/pretty-print-stack-trace actual test/*stack-trace-depth*))
      (prn actual))))
 
 (defmethod report :summary [{:keys [test pass fail error]}]
   (let [total (+ pass fail error)
-        color (if (= pass total) ansi/green ansi/red)]
+        color (if (= pass total) (:pass *fonts*) (:error *fonts*))]
     (test/with-test-out
       (newline)
       (println "Ran" test "tests containing" total "assertions.")
-      (println (color (str fail " failures, " error " errors."))))))
+      (println (str color fail " failures, " error " errors." (:reset *fonts*))))))
