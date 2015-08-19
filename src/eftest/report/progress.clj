ee0e3173b44a4c69f1b5fe7f19c6c9c43119b421
James Reeves <jreeves@weavejester.com>
2015-08-19 05:05:28 +0100

    Add progress methods for :pass, :fail and :error

--- src/eftest/report/progress.clj
+++ src/eftest/report/progress.clj
@@ -4,6 +4,8 @@
             [eftest.report.pretty :as pretty]
             [progrock.core :as prog]))
 
+(def ^:private clear-line (apply str "\r" (repeat 80 " ")))
+
 (defmulti report :type)
 
 (defmethod report :default [m])
@@ -11,6 +13,23 @@
 (defmethod report :begin-test-run [m]
   (test/with-test-out
     (prog/print (reset! runner/*context* (prog/progress-bar (:count m))))))
+
+(defmethod report :pass [m]
+  (pretty/report m))
+
+(defmethod report :fail [m]
+  (test/with-test-out
+    (print clear-line)
+    (pretty/report m)
+    (newline)
+    (prog/print @runner/*context*)))
+
+(defmethod report :error [m]
+  (test/with-test-out
+    (print clear-line)
+    (pretty/report m)
+    (newline)
+    (prog/print @runner/*context*)))
 
 (defmethod report :end-test-var [m]
   (test/with-test-out
