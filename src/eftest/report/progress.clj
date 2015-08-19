809a3b61ef9deab6409db67ea0917e2f31506dd9
James Reeves <jreeves@weavejester.com>
2015-08-19 01:31:41 +0100

    Add basic progress bar reporter

---
+++ src/eftest/report/progress.clj
@@ -0,0 +1,21 @@
+(ns eftest.report.progress
+  (:require [clojure.test :as test]
+            [eftest.runner :as runner]
+            [eftest.report.pretty :as pretty]
+            [progrock.core :as prog]))
+
+(defmulti report :type)
+
+(defmethod report :default [m])
+
+(defmethod report :begin-test-run [m]
+  (test/with-test-out
+    (prog/print (reset! runner/*context* (prog/progress-bar (:count m))))))
+
+(defmethod report :end-test-var [m]
+  (test/with-test-out
+    (prog/print (swap! runner/*context* prog/tick))))
+
+(defmethod report :summary [m]
+  (test/with-test-out
+    (prog/print (swap! runner/*context* prog/done))))
