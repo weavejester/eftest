c5d83e154ca3949d2ce7914ddb6f6ed25852dc26
James Reeves <jreeves@weavejester.com>
2015-08-20 02:21:12 +0100

    Make majority of runner functions private

--- src/eftest/runner.clj
+++ src/eftest/runner.clj
@@ -14,53 +14,47 @@
 
 (defmethod test/report :begin-test-run [_])
 
-(defn require-namespaces-in-dir [dir]
+(defn- require-namespaces-in-dir [dir]
   (map (fn [ns] (require ns) (find-ns ns)) (find/find-namespaces-in-dir (io/file dir))))
 
-(defn find-tests-in-namespace [ns]
+(defn- find-tests-in-namespace [ns]
   (->> ns ns-interns vals (filter (comp :test meta))))
 
-(defn find-tests-in-dir [dir]
+(defn- find-tests-in-dir [dir]
   (mapcat find-tests-in-namespace (require-namespaces-in-dir dir)))
 
-(defn synchronize [f]
+(defn- synchronize [f]
   (let [lock (Object.)] (fn [x] (locking lock (f x)))))
 
-(defn test-vars
-  ([vars] (test-vars vars {}))
-  ([vars opts]
-   (doseq [[ns vars] (group-by (comp :ns meta) vars)]
-     (let [once-fixtures (-> ns meta ::test/once-fixtures test/join-fixtures)
-           each-fixtures (-> ns meta ::test/each-fixtures test/join-fixtures)
-           report        (synchronize test/report)
-           test-var      (fn [v] (binding [test/report report] (test/test-var v)))]
-       (once-fixtures
-        (fn []
-          (if (:multithread? opts true)
-            (dorun (pmap (bound-fn [v] (each-fixtures #(test-var v))) vars))
-            (doseq [v vars] (each-fixtures #(test-var v))))))))))
+(defn- test-vars [vars opts]
+  (doseq [[ns vars] (group-by (comp :ns meta) vars)]
+    (let [once-fixtures (-> ns meta ::test/once-fixtures test/join-fixtures)
+          each-fixtures (-> ns meta ::test/each-fixtures test/join-fixtures)
+          report        (synchronize test/report)
+          test-var      (fn [v] (binding [test/report report] (test/test-var v)))]
+      (once-fixtures
+       (fn []
+         (if (:multithread? opts true)
+           (dorun (pmap (bound-fn [v] (each-fixtures #(test-var v))) vars))
+           (doseq [v vars] (each-fixtures #(test-var v)))))))))
 
 (defn- filter-vars [vars opts]
   (filter (:filter opts (constantly true)) vars))
 
-(defn test-ns
-  ([ns] (test-ns ns {}))
-  ([ns opts]
-   (let [ns (the-ns ns)]
-     (binding [test/*report-counters* (ref test/*initial-report-counters*)]
-       (test/do-report {:type :begin-test-ns, :ns ns})
-       (if-let [hook (find-var (symbol (str (ns-name ns)) "test-ns-hook"))]
-         ((var-get hook))
-         (test-vars (filter-vars (find-tests-in-namespace ns) opts) opts))
-       (test/do-report {:type :end-test-ns, :ns ns})
-       @test/*report-counters*))))
+(defn- test-ns [ns opts]
+  (let [ns (the-ns ns)]
+    (binding [test/*report-counters* (ref test/*initial-report-counters*)]
+      (test/do-report {:type :begin-test-ns, :ns ns})
+      (if-let [hook (find-var (symbol (str (ns-name ns)) "test-ns-hook"))]
+        ((var-get hook))
+        (test-vars (filter-vars (find-tests-in-namespace ns) opts) opts))
+      (test/do-report {:type :end-test-ns, :ns ns})
+      @test/*report-counters*)))
 
-(defn test-dir
-  ([dir] (test-dir dir {}))
-  ([dir opts]
-   (->> (require-namespaces-in-dir dir)
-        (map #(test-ns % opts))
-        (apply merge-with +))))
+(defn- test-dir [dir opts]
+  (->> (require-namespaces-in-dir dir)
+       (map #(test-ns % opts))
+       (apply merge-with +)))
 
 (defn- count-tests [dir opts]
   (count (filter-vars (find-tests-in-dir dir) opts)))
