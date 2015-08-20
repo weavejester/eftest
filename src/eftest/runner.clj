8abe7276a3bdd907e841398ee15a805eefd07cfa
James Reeves <jreeves@weavejester.com>
2015-08-20 03:08:08 +0100

    Change test runner to work off list of vars
    Drop support for test-ns-hook as it doesn't work with vars or filters,
    and it's rarely, if ever, used.

--- src/eftest/runner.clj
+++ src/eftest/runner.clj
@@ -14,58 +14,57 @@
 
 (defmethod test/report :begin-test-run [_])
 
-(defn- require-namespaces-in-dir [dir]
-  (map (fn [ns] (require ns) (find-ns ns)) (find/find-namespaces-in-dir (io/file dir))))
-
-(defn- find-tests-in-namespace [ns]
-  (->> ns ns-interns vals (filter (comp :test meta))))
-
-(defn- find-tests-in-dir [dir]
-  (mapcat find-tests-in-namespace (require-namespaces-in-dir dir)))
-
 (defn- synchronize [f]
   (let [lock (Object.)] (fn [x] (locking lock (f x)))))
 
-(defn- test-vars [vars opts]
-  (doseq [[ns vars] (group-by (comp :ns meta) vars)]
-    (let [once-fixtures (-> ns meta ::test/once-fixtures test/join-fixtures)
-          each-fixtures (-> ns meta ::test/each-fixtures test/join-fixtures)
-          report        (synchronize test/report)
-          test-var      (fn [v] (binding [test/report report] (test/test-var v)))]
-      (once-fixtures
-       (fn []
-         (if (:multithread? opts true)
-           (dorun (pmap (bound-fn [v] (each-fixtures #(test-var v))) vars))
-           (doseq [v vars] (each-fixtures #(test-var v)))))))))
+(defn- test-vars [ns vars opts]
+  (let [once-fixtures (-> ns meta ::test/once-fixtures test/join-fixtures)
+        each-fixtures (-> ns meta ::test/each-fixtures test/join-fixtures)
+        report        (synchronize test/report)
+        test-var      (fn [v] (binding [test/report report] (test/test-var v)))]
+    (once-fixtures
+     (fn []
+       (if (:multithread? opts true)
+         (dorun (pmap (bound-fn [v] (each-fixtures #(test-var v))) vars))
+         (doseq [v vars] (each-fixtures #(test-var v))))))))
 
-(defn- filter-vars [vars opts]
-  (filter (:filter opts (constantly true)) vars))
-
-(defn- test-ns [ns opts]
+(defn- test-ns [ns vars opts]
   (let [ns (the-ns ns)]
     (binding [test/*report-counters* (ref test/*initial-report-counters*)]
       (test/do-report {:type :begin-test-ns, :ns ns})
-      (if-let [hook (find-var (symbol (str (ns-name ns)) "test-ns-hook"))]
-        ((var-get hook))
-        (test-vars (filter-vars (find-tests-in-namespace ns) opts) opts))
+      (test-vars ns vars opts)
       (test/do-report {:type :end-test-ns, :ns ns})
       @test/*report-counters*)))
 
-(defn- test-dir [dir opts]
-  (->> (require-namespaces-in-dir dir)
-       (map #(test-ns % opts))
+(defn- test-all [vars opts]
+  (->> (group-by (comp :ns meta) vars)
+       (map (fn [[ns vars]] (test-ns ns vars opts)))
        (apply merge-with +)))
 
-(defn- count-tests [dir opts]
-  (count (filter-vars (find-tests-in-dir dir) opts)))
+(defn- require-namespaces-in-dir [dir]
+  (map (fn [ns] (require ns) (find-ns ns)) (find/find-namespaces-in-dir (io/file dir))))
+
+(defmulti find-tests type)
+
+(derive clojure.lang.Namespace ::namespace)
+(derive clojure.lang.Symbol    ::namespace)
+(derive java.io.File           ::directory)
+(derive java.lang.String       ::directory)
+
+(defmethod find-tests ::namespace [ns]
+  (->> ns ns-interns vals (filter (comp :test meta))))
+
+(defmethod find-tests ::directory [dir]
+  (mapcat find-tests-in-namespace (require-namespaces-in-dir dir)))
 
 (defn run-tests
-  ([dir] (run-tests dir {}))
-  ([dir opts]
-   (let [start-time (System/currentTimeMillis)]
+  ([tests] (run-tests tests {}))
+  ([tests opts]
+   (let [start-time (System/currentTimeMillis)
+         vars       (find-tests tests)]
      (binding [report/*context* (atom {})
                test/report      (:report opts progress/report)]
-       (test/do-report {:type :begin-test-run, :count (count-tests dir opts)})
-       (let [counters (test-dir dir opts)
+       (test/do-report {:type :begin-test-run, :count (count vars)})
+       (let [counters (test-all vars opts)
              duration (- (System/currentTimeMillis) start-time)]
          (test/do-report (assoc counters :type :summary, :duration duration)))))))
