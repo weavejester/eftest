cbba19088c86ce0d70645ea31836c78eeac4ce29
James Reeves <jreeves@weavejester.com>
2015-08-21 01:44:29 +0100

    Add description and URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject eftest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A fast and pretty test runner"
+  :url "https://github.com/weavejester/eftest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
