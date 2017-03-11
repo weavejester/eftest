c5537bc8bdc3a97876651c5be9c239ccd068f697
James Reeves <jreeves@weavejester.com>
2017-03-11 04:32:42 +0000

    Release 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eftest "0.1.3"
+(defproject eftest "0.1.4"
   :description "A fast and pretty test runner"
   :url "https://github.com/weavejester/eftest"
   :license {:name "Eclipse Public License"
