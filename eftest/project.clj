503a89e86036ec304efe732661a088da96bdc76a
kawas44 <kawas44@gmail.com>
2018-03-26 01:07:47 +0200

    Add main function for CLI usage
    - Allow execution from any tool that can launch from -main
      (lein run -m, clj -m, etc)
    
    weavejester/eftest/#43

--- eftest/project.clj
+++ eftest/project.clj
@@ -4,7 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.reader "1.2.2"]
                  [progrock "0.1.2"]
                  [io.aviso/pretty "0.1.34"]
-                 [mvxcvi/puget "1.0.2"]])
+                 [mvxcvi/puget "1.0.2"]]
+  :main eftest.main)
