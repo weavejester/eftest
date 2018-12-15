6f839aefc59f8ac3f4b8c8c244248e0d2818f532
James Reeves <jreeves@weavejester.com>
2018-12-15 12:50:41 +0000

    Release 0.5.4

--- lein-eftest/project.clj
+++ lein-eftest/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-eftest "0.5.3"
+(defproject lein-eftest "0.5.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- lein-eftest/src/leiningen/eftest.clj
+++ lein-eftest/src/leiningen/eftest.clj
@@ -5,7 +5,7 @@
             [leiningen.core.project :as project]))
 
 (def eftest-profile
-  {:dependencies '[[eftest "0.5.3"]]})
+  {:dependencies '[[eftest "0.5.4"]]})
 
 (defn- report-namespace [project]
   (if-let [reporter (get-in project [:eftest :report])]
