e812e8c7f828368e4ba4e89149ed31dc0f443e9b
James Reeves <jreeves@weavejester.com>
2019-05-19 00:23:56 +0100

    Release 0.5.8

--- lein-eftest/src/leiningen/eftest.clj
+++ lein-eftest/src/leiningen/eftest.clj
@@ -5,7 +5,7 @@
             [leiningen.core.project :as project]))
 
 (def eftest-profile
-  {:dependencies '[[eftest "0.5.7"]]})
+  {:dependencies '[[eftest "0.5.8"]]})
 
 (defn- report-namespace [project]
   (if-let [reporter (get-in project [:eftest :report])]
