33db0429b6ba597aa3caf0a6bb1fcb4d39c6818b
Sasha Gerrand <sgerrand@users.noreply.github.com>
2019-12-05 15:09:18 +0000

    Update project description and URL

--- lein-eftest/project.clj
+++ lein-eftest/project.clj
@@ -1,6 +1,6 @@
 (defproject lein-eftest "0.5.9"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A fast and pretty Clojure test runner"
+  :url "https://github.com/weavejester/eftest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
