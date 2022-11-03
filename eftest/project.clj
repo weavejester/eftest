2461de1f72b3a5247b5d8ccc641df0115cbe0bee
Max Penet <mpenet@s-exp.com>
2022-11-03 10:28:06 +0100

    prep for release

--- eftest/project.clj
+++ eftest/project.clj
@@ -1,4 +1,4 @@
-(defproject com.exoscale/eftest "0.5.9"
+(defproject com.exoscale/eftest "1.0.0-alpha1-SNAPSHOT"
   :description "A fast and pretty test runner"
   :url "https://github.com/exoscale/eftest"
   :license {:name "Eclipse Public License"
@@ -7,4 +7,13 @@
                  [org.clojure/tools.namespace "1.3.0"]
                  [progrock "0.1.2"]
                  [io.aviso/pretty "1.3"]
-                 [mvxcvi/puget "1.3.2"]])
+                 [mvxcvi/puget "1.3.2"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
