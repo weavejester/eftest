84bf19b3525b2f10a3c123014a332ec0bcc88121
James Reeves <jreeves@weavejester.com>
2022-11-10 10:51:29 +0000

    Add profile for Clojure 1.11

--- eftest/project.clj
+++ eftest/project.clj
@@ -9,8 +9,10 @@
                  [io.aviso/pretty "1.3"]
                  [mvxcvi/puget "1.3.4"]]
   :plugins [[lein-eftest "0.5.9"]]
-  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "eftest"]}
+  :aliases {"test-all"
+            ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "eftest"]}
   :profiles
   {:1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}})
