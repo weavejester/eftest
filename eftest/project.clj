17b7f77ba3787d81a8c4613d6fc7cd0bfc0e936b
James Reeves <jreeves@weavejester.com>
2022-11-10 10:51:13 +0000

    Update dependencies

--- eftest/project.clj
+++ eftest/project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.namespace "1.3.0"]
                  [progrock "0.1.2"]
-                 [io.aviso/pretty "0.1.34"]
-                 [mvxcvi/puget "1.1.1"]]
+                 [io.aviso/pretty "1.3"]
+                 [mvxcvi/puget "1.3.4"]]
   :plugins [[lein-eftest "0.5.9"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "eftest"]}
   :profiles
