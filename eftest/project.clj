57cd4c39a3a538d8e07e4d199f8057035dfc1eb6
Sang-Kyu Park <psk810@gmail.com>
2023-01-24 20:02:42 +0900

    version up : 0.6.0 -> 0.6.2

--- eftest/project.clj
+++ eftest/project.clj
@@ -1,6 +1,6 @@
-(defproject eftest "0.6.0"
+(defproject org.clojars.guruma/eftest "0.6.2"
   :description "A fast and pretty test runner"
-  :url "https://github.com/weavejester/eftest"
+  :url "https://github.com/greenchemsoft/eftest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
@@ -9,7 +9,7 @@
                  [io.aviso/pretty "1.3"]
                  [mvxcvi/puget "1.3.4"]
                  [juji/editscript "0.6.2"]]
-  :plugins [[lein-eftest "0.6.0"]]
+  :plugins [[org.clojars.guruma/lein-eftest "0.6.2"]]
   :aliases {"test-all"
             ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "eftest"]}
   :profiles
--- lein-eftest/project.clj
+++ lein-eftest/project.clj
@@ -1,6 +1,6 @@
-(defproject lein-eftest "0.6.0"
+(defproject org.clojars.guruma/lein-eftest "0.6.2"
   :description "A fast and pretty Clojure test runner"
-  :url "https://github.com/weavejester/eftest"
+  :url "https://github.com/greenchemsoft/eftest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
